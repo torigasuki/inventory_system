cc5190c41d2804d560c68f71f1bb4e5e929e7727
Jeong SeungHo <teapackage16@gmail.com>
2023-04-07 21:00:15 +0900

    first commit

--- ims/ims/settings.py
+++ ims/ims/settings.py
@@ -11,16 +11,18 @@ https://docs.djangoproject.com/en/4.2/ref/settings/
 """
 
 from pathlib import Path
-
+from decouple import config
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
 
 
+
+SECRET_KEY = config("SECRET_KEY")
 # Quick-start development settings - unsuitable for production
 # See https://docs.djangoproject.com/en/4.2/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'django-insecure-wmqn_py#^gfp!t-a6#nixfuv*my)$l=rxm%lnsff_(#w1@*ljk'
+
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
